4ff6ca50e16d81fdcdb11264062120bcce62e36c
Akobir <akobirabduvahobov@gmail.com>
2025-01-25 12:11:17 +0500

    3.7 dars. Created

--- ConsoleApp3.6/ConsoleApp3.6/MusicCrudApiBroker.cs
+++ ConsoleApp3.6/ConsoleApp3.6/MusicCrudApiBroker.cs
@@ -12,8 +12,70 @@ public class MusicCrudApiBroker
     {
         _baseUrl = "http://localhost:5044/api/music";
         _httpClient = new HttpClient();
-        Add();
+        //Add();
         GetAll();
+        //GetById();
+        //Delete();
+        //Update();
+    }
+
+
+    public void Update()
+    {
+        var url = $"{_baseUrl}/updateMusic";
+        var music = new Music()
+        {
+            Id = new Guid("97b02f78-c678-466a-a2f5-2a14fa66ac95"),
+            Name = "Esindan chiqar",
+            MB = 9,
+            AuthorName = "Ummon",
+            Description = "azoblash",
+            QuentityLikes = 200
+        };
+
+        var json = JsonSerializer.Serialize(music);
+        StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+
+        var response = _httpClient.PutAsync(url, content).Result;
+        response.EnsureSuccessStatusCode();
+
+        var responseContent = response.Content.ReadAsStringAsync().Result;
+        Console.WriteLine(responseContent);
+    }
+
+    public void Delete()
+    {
+        Guid id = new Guid("0c78696f-15f4-4784-8b5f-8f0e03b4f287");
+
+        var url = $"{_baseUrl}/deleteMusic/{id}";
+
+        HttpResponseMessage response = _httpClient.DeleteAsync(url).Result;
+
+        response.EnsureSuccessStatusCode();
+
+        string responseContent = response.Content.ReadAsStringAsync().Result;
+
+        var finish = 0;
+    }
+
+    public void GetById()
+    {
+        Guid id = new Guid("0c78696f-15f4-4784-8b5f-8f0e03b4f287");
+        var url = $"{_baseUrl}/getMusicById/{id}";
+
+        HttpResponseMessage response = _httpClient.GetAsync(url).Result;
+
+        response.EnsureSuccessStatusCode();
+
+        string responseContent = response.Content.ReadAsStringAsync().Result;
+
+        JsonSerializerOptions options = new JsonSerializerOptions();
+        options.PropertyNameCaseInsensitive = true;
+
+        var music = JsonSerializer.Deserialize<Music>(responseContent, options);
+
+        Console.WriteLine(music);
     }
 
     public void GetAll()
---
+++ ConsoleApp3.7/ConsoleApp3.7/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Diagnostics;
+
+namespace ConsoleApp3._7;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Main is started");
+        //Console.WriteLine($"ThredId : {Thread.CurrentThread.ManagedThreadId}");
+
+        //Thread thread1 = new Thread(CountUp)
+        //{
+        //    Name = "oshpaz1"
+        //};
+
+        //Thread thread2 = new Thread(CountDown)
+        //{
+        //    Name = "ozshpaz2"
+        //};
+
+        //thread1.Start();
+        //thread2.Start();
+
+        //thread1.Join();
+        //thread2.Join();
+
+        //Console.WriteLine("Main is finished");
+        //Console.WriteLine($"ThredId : {Thread.CurrentThread.ManagedThreadId}");
+
+        string diroctoryPath = @"D:\Work\Groups\G_10_C#\Classwork\3.7 tasks";
+        var filesPath = Directory.GetFiles(diroctoryPath);
+
+        var finish = 0;
+        //Stopwatch stopwatch = new Stopwatch();
+        //stopwatch.Start();
+
+        //Console.WriteLine("Main is started");
+        //Thread thread1 = new Thread(() => IncrementNumber("salom"));
+        //Thread thread2 = new Thread(() => IncrementNumber("salom"));
+
+        //thread1.Start();
+        //thread2.Start();
+
+        //thread1.Join();
+        //thread2.Join();
+
+        //Console.WriteLine($"Finish : {_counter}");
+        //stopwatch.Stop();
+        //Console.WriteLine(stopwatch.ToString());
+    }
+
+    public static int _counter = 0;
+    public static Object _lock = new Object();
+
+    public static void IncrementNumber(string filePath)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            lock (_lock)
+            {
+                ++_counter;
+                Console.WriteLine("counter : " + _counter);
+                Console.WriteLine($"ThreadId : {Thread.CurrentThread.ManagedThreadId}");
+                Console.WriteLine($"ThreadId : {Thread.CurrentThread.ThreadState}");
+            }
+
+            Thread.Sleep(1000);
+        }
+    }
+
+    //public static void CountUp()
+    //{
+    //    for (int i = 0; i <= 10; i++)
+    //    {
+    //        Console.WriteLine($"{i} : CountUp, ThredId : " +
+    //            $"{Thread.CurrentThread.ManagedThreadId}, {Thread.CurrentThread.Name}");
+    //        Thread.Sleep(1000);
+    //    }
+    //}
+
+    //public static void CountDown()
+    //{
+    //    for (int i = 10; i >= 0; i--)
+    //    {
+    //        Console.WriteLine($"{i} : CountDown, ThredId : " +
+    //            $"{Thread.CurrentThread.ManagedThreadId}, {Thread.CurrentThread.Name}");
+    //        Thread.Sleep(1000);
+    //    }
+    //}
+}
