46e85cc2524eb16cda7b654e07d41c5de713ca15
Akobir <akobirabduvahobov@gmail.com>
2025-01-25 15:19:40 +0500

    3.7

--- ConsoleApp3.7/ConsoleApp3.7/Program.cs
+++ ConsoleApp3.7/ConsoleApp3.7/Program.cs
@@ -1,73 +1,133 @@
-﻿using System.Diagnostics;
+﻿using System.Text.RegularExpressions;
 
 namespace ConsoleApp3._7;
 
 internal class Program
 {
+
     static void Main(string[] args)
     {
-        //Console.WriteLine("Main is started");
-        //Console.WriteLine($"ThredId : {Thread.CurrentThread.ManagedThreadId}");
+        //var drPath = @"D:\Work\Groups\G_10_C#\Classwork\3.7_tasks";
 
-        //Thread thread1 = new Thread(CountUp)
+        //var files = Directory.GetFiles(drPath).ToList();
+
+        //foreach (var file in files)
         //{
-        //    Name = "oshpaz1"
-        //};
+        //    if (file.Contains("results")) continue;
+        //    var thread = new Thread(() => WriteNumberOfDigitsToFile(file));
+        //    thread.Start();
+        //}
 
-        //Thread thread2 = new Thread(CountDown)
-        //{
-        //    Name = "ozshpaz2"
-        //};
+        Console.WriteLine(ValidateName("Akobir"));
+        Console.WriteLine(ValidateName("hello"));
+        Console.WriteLine(ValidateName("Akobir212"));
+        Console.WriteLine(ValidateName("Ako bir"));
+        Console.WriteLine(ValidateName("AKobir"));
+        Console.WriteLine(ValidateName("Umar"));
 
-        //thread1.Start();
-        //thread2.Start();
-
-        //thread1.Join();
-        //thread2.Join();
-
-        //Console.WriteLine("Main is finished");
-        //Console.WriteLine($"ThredId : {Thread.CurrentThread.ManagedThreadId}");
-
-        string diroctoryPath = @"D:\Work\Groups\G_10_C#\Classwork\3.7 tasks";
-        var filesPath = Directory.GetFiles(diroctoryPath);
-
-        var finish = 0;
-        //Stopwatch stopwatch = new Stopwatch();
-        //stopwatch.Start();
-
-        //Console.WriteLine("Main is started");
-        //Thread thread1 = new Thread(() => IncrementNumber("salom"));
-        //Thread thread2 = new Thread(() => IncrementNumber("salom"));
-
-        //thread1.Start();
-        //thread2.Start();
-
-        //thread1.Join();
-        //thread2.Join();
-
-        //Console.WriteLine($"Finish : {_counter}");
-        //stopwatch.Stop();
-        //Console.WriteLine(stopwatch.ToString());
     }
 
-    public static int _counter = 0;
-    public static Object _lock = new Object();
-
-    public static void IncrementNumber(string filePath)
+    public static bool ValidateName(string name)
     {
-        for (int i = 0; i < 10; i++)
+        string namePattern = @"^[A-Z]{1}[a-z]+$";
+        var res = Regex.IsMatch(name, namePattern);
+        return res;
+    }
+
+
+
+
+    public static void WriteNumberOfDigitsToFile(string filePath)
+    {
+        var resultPath = @"D:\Work\Groups\G_10_C#\Classwork\3.7_tasks\results.txt";
+        lock (_lock)
         {
-            lock (_lock)
+            if (!File.Exists(filePath))
             {
-                ++_counter;
-                Console.WriteLine("counter : " + _counter);
-                Console.WriteLine($"ThreadId : {Thread.CurrentThread.ManagedThreadId}");
-                Console.WriteLine($"ThreadId : {Thread.CurrentThread.ThreadState}");
+                File.Create(filePath).Close();
             }
 
-            Thread.Sleep(1000);
+            using (var sr = new StreamReader(filePath))
+            {
+                var lines = sr.ReadToEnd();
+                var count = lines.Count(ch => Char.IsDigit(ch));
+                using (StreamWriter stream = new StreamWriter(resultPath, true))
+                {
+                    var id = Thread.CurrentThread.ManagedThreadId;
+                    var line = $"ThreadId : {id}, Path : {filePath}, Count : {count}";
+                    stream.WriteLine(line);
+                }
+            }
         }
     }
+
+
+    public static Object _lock = new Object();
+
+    //static void Main(string[] args)
+    //{
+    //    //Console.WriteLine("Main is started");
+    //    //Console.WriteLine($"ThredId : {Thread.CurrentThread.ManagedThreadId}");
+
+    //    //Thread thread1 = new Thread(CountUp)
+    //    //{
+    //    //    Name = "oshpaz1"
+    //    //};
+
+    //    //Thread thread2 = new Thread(CountDown)
+    //    //{
+    //    //    Name = "ozshpaz2"
+    //    //};
+
+    //    //thread1.Start();
+    //    //thread2.Start();
+
+    //    //thread1.Join();
+    //    //thread2.Join();
+
+    //    //Console.WriteLine("Main is finished");
+    //    //Console.WriteLine($"ThredId : {Thread.CurrentThread.ManagedThreadId}");
+
+    //    string diroctoryPath = @"D:\Work\Groups\G_10_C#\Classwork\3.7 tasks";
+    //    var filesPath = Directory.GetFiles(diroctoryPath);
+
+    //    var finish = 0;
+    //    //Stopwatch stopwatch = new Stopwatch();
+    //    //stopwatch.Start();
+
+    //    //Console.WriteLine("Main is started");
+    //    //Thread thread1 = new Thread(() => IncrementNumber("salom"));
+    //    //Thread thread2 = new Thread(() => IncrementNumber("salom"));
+
+    //    //thread1.Start();
+    //    //thread2.Start();
+
+    //    //thread1.Join();
+    //    //thread2.Join();
+
+    //    //Console.WriteLine($"Finish : {_counter}");
+    //    //stopwatch.Stop();
+    //    //Console.WriteLine(stopwatch.ToString());
+    //}
+
+    //public static int _counter = 0;
+    //public static Object _lock = new Object();
+
+    //public static void IncrementNumber(string filePath)
+    //{
+    //    for (int i = 0; i < 10; i++)
+    //    {
+    //        lock (_lock)
+    //        {
+    //            ++_counter;
+    //            Console.WriteLine("counter : " + _counter);
+    //            Console.WriteLine($"ThreadId : {Thread.CurrentThread.ManagedThreadId}");
+    //            Console.WriteLine($"ThreadId : {Thread.CurrentThread.ThreadState}");
+    //        }
+
+    //        Thread.Sleep(1000);
+    //    }
+    //}
 
     //public static void CountUp()
     //{
